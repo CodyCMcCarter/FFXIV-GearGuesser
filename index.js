896ed44c25995e6961e7eabe78324682d2e93132
Cody McCarter <codycmccarter@gmail.com>
2024-05-18 21:48:50 -0400

    Add fail state, success state, and 2 more clue states

--- index.js
+++ index.js
@@ -11,29 +11,56 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 var gearInfo;
 var gearImage;
+var guesses = [];
+var statNames = [];
+var statValues = [];
+var result;
 
 app.get("/", async (req, res) => {
-    res.render("index.ejs", { gearInfo: gearInfo, gearImage: gearImage });
+    res.render("index.ejs", { 
+        gearInfo: gearInfo, 
+        gearImage: gearImage, 
+        guesses: guesses, 
+        statNames: statNames, 
+        statValues: statValues, 
+        result: result
+    });
 });
 
 app.get("/startGame", async (req, res) => {
-    let gearId = Math.floor((Math.random()*1000)+2937);
+    let gearId = Math.floor((Math.random()*100)+34830);
     let response = await axios.get(API_URL + "/item/"+gearId);
     gearInfo = response.data;
+    let stats = gearInfo.Stats;
+    statNames = Object.keys(stats);
+    for(var i = 0; i < statNames.length; i++){
+        statValues.push(stats[statNames[i]].NQ);
+    }
     let imageResponse = await axios.get(API_URL + gearInfo.IconHD, { responseType: "arraybuffer" });
     let buffer = Buffer.from(imageResponse.data, 'binary').toString("base64");
     gearImage = `data:${imageResponse.headers["content-type"]};base64,${buffer}`;
     res.redirect("/");
 });
 
-app.get("/checkGuess", (req, res) => {
-    
+app.post("/checkGuess", (req, res) => {
+    console.log(guesses.length);
+    guesses.push(req.body.guess);
+    if (req.body.guess === gearInfo.Name) {
+        result = "Duty Complete!";
+    } else if (guesses.length >= 5) {
+        result = "Duty Failed..."
+    }
+    res.redirect("/");
 });
 
-app.get("/endGame", (req, res) => {
+app.post("/endGame", (req, res) => {
     gearInfo = null;
     gearImage = null;
-    res.redirect("/");
+    guesses = [];
+    statNames = [];
+    statValues = [];
+    result = null;
+    res.redirect("/startGame");
 });
 
 app.listen(port, () => {
